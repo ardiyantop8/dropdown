20c224013dc0b9beaeafcdcec062413b0839402e
Ardiyanto <ardiyantop8@gmail.com>
2023-01-15 15:02:51 +0700

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Select from 'react-select'
+import {useEffect, useState } from 'react';
 
 function App() {
+  const [datas, setDatas] = useState([])
+  const [userSelect, setUserSelect] = useState("") /* membuat variable const dengan yg kiri adalah tempat nilainya dan yg kanan adalah untuk mensetnya dengan metode useState */
+  const [isShow, setIsShow] = useState(false) 
+  
+  const getBerries = async () => {
+    const berries = await fetch("https://pokeapi.co/api/v2/berry/")
+    const value = await berries.json()
+    let result = value.results.map(data => {
+      return {
+        label : data.name,
+        value : data.name
+      }
+    })
+    setDatas(result.sort((a,b) => a.label.localeCompare(b.label))) /* sort untuk mengurutkan data secara ascending */
+  }
+
+  useEffect(() => {
+    getBerries()
+  }, [])
+
+  const handleSubmit = () => {
+    // console.log('Nilai yg dipilih = ', userSelect) /* Untuk menampilkan hasilnya dengan userSelect */
+    setIsShow(state => !state)
+  }
+
+  const handleChange = (value) => {
+    setUserSelect(value) /* Untuk mengeset value yg dipilih kedalam setUserSelect */
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => handleSubmit()} disabled={!userSelect}>{isShow ? "Hide Button" : "Show Values"}</button>
+      <Select options={datas} onChange={(e) => handleChange(e.value)}></Select>
+      <h1>Nilai yang dipilih adalah : {isShow ? userSelect : ""}</h1>
     </div>
   );
 }
